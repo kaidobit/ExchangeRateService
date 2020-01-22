7a7fa946c3a6f0ad52961a0a605fa0aeaf3036b0
Patrick Shaikh <patrick.shaikh@hotmail.de>
2020-01-22 18:18:19 +0100

    create project

---
+++ src/test/java/com/pshaikh/exchange_rate_service/ExchangeRateServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pshaikh.exchange_rate_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExchangeRateServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
